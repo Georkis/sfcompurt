34134c319e42e5dab15e531a5445ba4bfa53b3e3
georkis <georkis.santiesteban@gmail.com>
2024-04-24 13:57:24 -0400

    Modificando el app.js

--- assets/app.js
+++ assets/app.js
@@ -2,7 +2,6 @@ import './bootstrap.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './vendor/bootstrap/dist/bootstrap.bundle.min.js';
 import './js/color-modes.js';
-import './js/wow.js';
 
 /*
  * Welcome to your app's main JavaScript file!
@@ -12,6 +11,5 @@ import './js/wow.js';
  */
 import './styles/app.css';
 import './styles/carousel.css';
-import './styles/animate.css';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
